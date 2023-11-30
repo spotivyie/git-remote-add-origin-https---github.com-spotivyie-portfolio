66a4764d61f1f90367198d519a9de95b62b428c0
Eduarda Cardoso Brandão <eduardac.brandao@hotmail.com>
2023-11-30 17:48:42 -0400

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/SideBar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Title'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/spotivyie.png" />
+    <Titulo fontSize={20}>Eduarda Cardoso</Titulo>
+  </aside>
+)
+
+export default Sidebar
