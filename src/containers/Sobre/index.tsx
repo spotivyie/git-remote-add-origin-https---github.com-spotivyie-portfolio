ca2013b4726314569a3f223448486d9d3919857c
Eduarda Cardoso <eduardac.brandao@hotmail.com>
2024-02-06 21:35:36 -0400

    atualizando

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,10 +6,14 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero
-      inventore, quaerat necessitatibus repudiandae ex? Eligendi in impedit,
-      illo recusandae distinctio, enim commodi delectus harum tempore suscipit
-      dolor nemo nam?
+      Comecei no mundo da programação há aproximadamente 1 ano e desde então,
+      venho sempre buscando consolidar o que venho aprendendo e inovar através
+      de projetos pessoais. Estou aprofundando meus conhecimentos por meio de
+      cursos especializados na plataforma EBAC, o que me permite acompanhar as
+      tendências e práticas atuais no campo da programação web, com foco
+      principal em front-end, também tenho conhecimentos básicos em back-end.
+      Tenho uma série de qualidades pessoais, uma mentalidade colaborativa,
+      resiliência, criatividade e inovação.
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=spotivyie&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
